78b113d3535c6da4625e8e36f4b1d51b7843d696
SHASHANK DWIVEDI <dwivedishashank673@gmail.com>
2026-09-18 20:26:58 +0530

    Time: 11 ms (49.87%) | Memory: 47.9 MB (88.24%) - LeetSync

--- 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.java
+++ 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.java
@@ -5,34 +5,26 @@ class Solution {
         int n = s.length();
         List<String> ans = new ArrayList<>();
 
-        // leftmost[i] = leftmost index of ('a' + i)
         int[] leftmost = new int[26];
         Arrays.fill(leftmost, n);
 
-        // rightmost[i] = rightmost index of ('a' + i)
         int[] rightmost = new int[26];
         Arrays.fill(rightmost, -1);
 
-        // Find leftmost and rightmost positions
         for (int i = 0; i < n; i++) {
             int index = s.charAt(i) - 'a';
             leftmost[index] = Math.min(leftmost[index], i);
             rightmost[index] = i;
         }
-
-        // Equivalent of C++ lambda getNewRight
         java.util.function.IntUnaryOperator getNewRight = (i) -> {
             int right = rightmost[s.charAt(i) - 'a'];
 
             for (int j = i; j <= right; j++) {
                 int index = s.charAt(j) - 'a';
 
-                // If this character appeared before i,
-                // this substring is invalid
                 if (leftmost[index] < i)
                     return -1;
 
-                // Expand right dynamically
                 right = Math.max(right, rightmost[index]);
             }
 
@@ -43,7 +35,6 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
 
-            // Current index is the first appearance of this character
             if (i == leftmost[s.charAt(i) - 'a']) {
 
                 int newRight = getNewRight.applyAsInt(i);
